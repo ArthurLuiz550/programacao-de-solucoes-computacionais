41dd94849db2261b31761e30c9b04bda9f68f632
Arthur Luiz <142808124+ArthurLuiz550@users.noreply.github.com>
2023-09-11 17:06:39 -0300

    Add files via upload

---
+++ práticaJava.java
@@ -0,0 +1,12 @@
+package pratica;
+
+import javax.swing.JOptionPane;
+
+public class práticaJava {
+
+	public static void main(String args[]) {
+	JOptionPane.showMessageDialog(null,"Exemplo de texto em uma janela!");
+	System.exit(0);
+
+	}
+}
